af347c4bb250e1dba549b3c823594d0602e4d481
Shubhra <128348252+iShubhra@users.noreply.github.com>
2026-08-06 23:13:40 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort( people.begin(), people.end() );
+        int left = 0, right = people.size() - 1, count = 0;
+        while ( left <= right ) {
+            if ( people[right] == limit ){
+                count++;
+                continue;
+            }
+
+            if ( people[left] + people[right] <= limit ) {
+                count++;
+                left++;
+                right--;
+                continue;
+            }
+
+            count++;
+            right--;
+        }
+
+        return count;
+    }
+};
