70d86959d5a902174c3c694093ccfdfbe6c84b85
Shubhra <128348252+iShubhra@users.noreply.github.com>
2026-08-08 00:16:06 +0530

    Add: subtree-of-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-2.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        // if ( root == nullptr && subRoot == nullptr ){
+        //     return true;
+        // }
+
+        // if ( root == nullptr && subRoot != nullptr || root != nullptr && subRoot == nullptr ) {
+        //     return false;
+        // }
+
+        // if ( root->val == subRoot->val && isSubtree(root->left, subRoot->left) && isSubtree(root->right, subRoot->right) ) {
+        //     return true;
+        // }
+
+        if ( root == NULL ) {
+            return false;
+        }
+
+        if ( isSameTree(root, subRoot) ) {
+            return true;
+        }
+
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if ( p == nullptr && q == nullptr ){
+            return true;
+        }else if ( p != nullptr && q == nullptr || p == nullptr && q != nullptr ){
+            return false;
+        }else if ( p->val != q->val ){
+            return false;
+        }
+
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
