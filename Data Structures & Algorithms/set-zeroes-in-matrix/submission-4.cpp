1af24814292d67cb2ce40f1f4e598aeb97f647b8
Shubhra <128348252+iShubhra@users.noreply.github.com>
2026-08-08 21:34:08 +0530

    Add: set-zeroes-in-matrix - submission-4

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-4.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+
+        // set first row & first col as 0
+        bool isFirstColZero = false, isFirstRowZero = false;
+        for ( int i = 0; i < matrix.size(); i++ ){
+            for ( int j = 0; j < matrix[0].size(); j++ ) {
+                if ( i == 0 && matrix[i][j] == 0 ){
+                    isFirstColZero = true;
+                }
+
+                if ( j == 0 && matrix[i][j] == 0 ){
+                    isFirstRowZero = true;
+                }
+
+                if ( matrix[i][j] == 0 ) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // print
+        //         for ( int i = 0; i < matrix.size(); i++ ){
+        //     for ( int j = 0; j < matrix[0].size(); j++ ) {
+        //         cout << matrix[i][j] << " " ;
+        //     }
+        //     cout << endl;
+        // }
+        //
+
+        // start row & col from 1 
+        for ( int i = 1; i < matrix.size(); i++ ){
+            for ( int j = 1; j < matrix[0].size(); j++ ) {
+                if ( matrix[i][0] == 0 || matrix[0][j] == 0 ){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // print
+        //     cout << endl;
+
+        //                 for ( int i = 0; i < matrix.size(); i++ ){
+        //     for ( int j = 0; j < matrix[0].size(); j++ ) {
+        //         cout << matrix[i][j] << " " ;
+        //     }
+        //     cout << endl;
+        // }
+        //
+
+       // cout << isFirstColZero << "  " << isFirstRowZero << endl;
+
+        // we have mark first row & col mark as zero if its req
+        if ( isFirstColZero ){
+            for ( int i = 0; i < matrix[0].size(); i++ ) {
+                matrix[0][i] = 0;
+            }
+        }
+
+        if ( isFirstRowZero ){
+            for ( int i = 0; i < matrix.size(); i++ ) {
+                matrix[i][0] = 0;
+            }
+        }
+
+
+        // cout << endl;
+
+        //                 for ( int i = 0; i < matrix.size(); i++ ){
+        //     for ( int j = 0; j < matrix[0].size(); j++ ) {
+        //         cout << matrix[i][j] << " " ;
+        //     }
+        //     cout << endl;
+        // }
+
+    }
+};
