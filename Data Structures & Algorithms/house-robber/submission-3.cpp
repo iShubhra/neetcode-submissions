62e140988ee08e2eec481d6f74e2a453a52d4e4f
Shubhra <128348252+iShubhra@users.noreply.github.com>
2026-08-12 14:51:58 +0530

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    unordered_map<int, int>m;
+    int rob(vector<int>& nums) {
+        int result = 0;
+        for ( int i = 0; i < nums.size(); i++ ){
+            auto it = m.find(i);
+            if ( it != m.end() ){
+                result = max(it->second, result);
+            }else {
+                result = max(result, robHouse(nums, i, nums[i]));
+            }
+        }
+
+        return result;
+    }
+
+    int robHouse(vector<int>& nums, int index, int robAmount) {
+        auto it = m.find(index);
+        if ( it != m.end() ) return it->second + robAmount;
+
+        int result = robAmount, maxValue ;
+        for ( int i = index + 2; i < nums.size(); i++ ){
+            auto it = m.find(i);
+            if ( it != m.end() ){
+                maxValue = it->second + robAmount;
+            }else {
+                maxValue =  robHouse(nums, i , robAmount + nums[i]);
+                m[i] = maxValue - robAmount;
+            }
+            result = max(result, maxValue);
+        }
+
+        return result;
+    }
+};
